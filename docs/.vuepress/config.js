f9a320101b6532415693846227072a909bdf7afa
yangyang1900 <2440286217@qq.com>
2021-10-03 21:52:16 +0800

    修改内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,32 @@
 module.exports = {
   title: "My Blog",
   description: "This is a blog.",
-  base: '/vuepreseelog/' 
+  base: '/vuepreseelog/',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+    nav: [
+      {
+        text: 'HTML/CSS',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'html', link: '/html' },
+          { text: 'css', link: '/css' }
+        ]
+      },
+      { text: 'JavaScript', link: '/JavaScript' }
+    ],
+    sidebar: {
+      '/html': [
+        '',
+      ],
+
+      '/css': [
+        '', 
+      ],
+
+      '/JavaScript': [
+        '',        
+      ]
+    }
+  }, 
 }
